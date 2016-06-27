d0c14e6f29e3cc47298ad041ce80e5323e9711e7
egmfilho <edumig12@hotmail.com>
2016-06-27 11:20:23 -0300

    Directive - Redireciona

---
+++ app/scripts/directives/redireciona.js
@@ -0,0 +1,24 @@
+/**
+ * Created by egmfilho on 21/06/16.
+ */
+
+'use strict';
+
+angular.module('commercialApp')
+    .directive('redireciona', ['$location', function($location) {
+
+    return function(scope, element, attrs) {
+      var path;
+
+      attrs.$observe('redireciona', function(val) {
+        path = val;
+      });
+
+      element.bind('click', function() {
+        scope.$apply(function() {
+          $location.path(path);
+        });
+      });
+    };
+
+  }]);
