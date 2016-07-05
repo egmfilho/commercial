6aa574717ecc868d696322673eace961b79e8800
egmfilho <edumig12@hotmail.com>
2016-07-05 17:37:16 -0300

    Constantes

---
+++ app/scripts/constants/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Created by egmfilho on 05/07/16.
+ */
+
+'use strict';
+
+angular.module('commercialApp', [])
+  .constant('RESPOSTAS', {
+    usuario_nao_encontrado: 404,
+    senha_incorreta: 0
+  });
